5859b6e3451c77e77681dba2902ecb9593c1237a
rakib hasan <dev.rakibhasan1@gmail.com>
2025-08-17 19:48:58 +0600

    added splashCursor in root layout

--- src/Layouts/RootLayouts.jsx
+++ src/Layouts/RootLayouts.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import SplashCursor from "../../reactbits/SplashCursor/SplashCursor"
 
 const RootLayouts = () => {
   return (
     <div>
+      <SplashCursor/>
       <Navbar></Navbar>
       <section className="min-h-screen">
         <Outlet></Outlet>
